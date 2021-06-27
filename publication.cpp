f3cc7ccf11b75d914b7912d28c75acedefab87c0
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-06-27 12:38:10 +0530

    added sales base class

--- publication.cpp
+++ publication.cpp
@@ -18,32 +18,54 @@ public:
   }
 };
 
-class book : public publication{
+class sales{
+  float sales[3];
+public:
+  void getdata(){
+    cout<<"Enter sales of 1st month: ";
+    cin>>sales[0];
+    cout<<"\nEnter sales of 2nd month: ";
+    cin>>sales[1];
+    cout<<"\nEnter sales of 3rd month: ";
+    cin>>sales[2];
+  }
+void putdata() const{
+  cout<<"Sales of 1st month: "<<sales[0]<<endl;
+  cout<<"Sales of 2nd month: "<<sales[1]<<endl;
+  cout<<"Sales of 3rd month: "<<sales[2]<<endl;
+}
+};
+
+class book : public publication, public sales{
   int pgcount;
 public:
   void getdata(){
     publication::getdata();
+    sales::getdata();
     cout<<"Enter number of pages in book:"<<endl;
     cin>>pgcount;
   }
 
   void putdata() const{
     publication::putdata();
+    sales::putdata();
     cout<<"Number of pages in book: "<<pgcount<<endl;
   }
 
 };
 
-class tape : public publication{
+class tape : public publication, public sales{
   float plytime;
 public:
   void getdata(){
     publication::getdata();
+    sales::getdata();
     cout<<"Enter playing time of tape: "<<endl;
     cin>>plytime;
   }
   void putdata() const{
     publication::putdata();
+    sales::putdata();
     cout<<"Playing time of tape: "<<plytime<<endl;
   }
 };

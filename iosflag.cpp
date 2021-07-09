b8c40b55ddda473efc9e6182d95b3a7d81cc21fb
seikhchilli <saurabhkrsingh2001@gmail.com>
2021-07-09 09:49:01 +0530

    commit

---
+++ iosflag.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  //cout<<endl;
+  cout.setf(ios::right);//left justify output text
+  cout<<"Hila\nmdisns\nwubjnee\n";
+  cout.unsetf(ios::right);//return to default
+
+
+  return 0;
+
+
+}
